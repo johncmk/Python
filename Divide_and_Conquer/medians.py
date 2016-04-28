2cd92ebb4303e99408cc63f2d420d07317ce7879
John <changmin_k@yahoo.com>
2016-04-27 23:53:28 -0400

    Collect medians in running

--- Divide_and_Conquer/medians.py
+++ Divide_and_Conquer/medians.py
@@ -1,34 +1,60 @@
 import heapq
-m = [] # median numbers
 
-'''
-Find median number in data stream
-O(n) linear algorithm'''
-def medians(li, minH = [], maxH = []):
+def get_median(max_h,min_h):
+    
+    heapq._heapify_max(max_h)
+    heapq.heapify(min_h)
+    
+    if len(max_h) == len(min_h):
+        temp = (max_h[0] + min_h[0])/2.0
+    elif len(max_h) - len(min_h) == 1:
+        temp = max_h[0]
+    elif len(min_h) - len(max_h) == 1:
+        temp = min_h[0]
+    else: 
+        if len(max_h) - len(min_h) > 1:
+            heapq.heappush(min_h,heapq.heappop(max_h))
+        elif len(min_h) - len(max_h) > 1:
+            heapq.heappush(max_h,heapq.heappop(min_h))
+        return get_median(max_h,min_h)
+    return temp
+        
+def medians(li):
+    
     if len(li) <= 1:
         return li
-    for i,el in enumerate(li):
-        if i == 0:
-            heapq.heappush(minH,el)
-            m.append(minH[0])
-        else:
-            md = m[-1]
-            heapq.heappush(minH,el) if el >= md else heapq.heappush(maxH,el)  # @NoEffect
-        heapq.heapify(minH)
-        heapq._heapify_max(maxH)
-        if i > 0:
-            if len(minH) == len(maxH):
-                m.append((minH[0] + maxH[0])/2.0)
-            elif len(minH) - len(maxH) == 1:
-                m.append(minH[0])
-            elif len(maxH) - len(minH) == 1:
-                m.append(maxH[0])
+    
+    min_h = [] # right side
+    max_h = [] # left side
+    li_m = [] # collect the medians 
+    
+    for el in li:
+        if max_h == [] and min_h == []:
+            temp = el
+            heapq.heappush(max_h,el)
+        else:    
+            mid = li_m[len(li_m)-1]
+            
+            if el <= mid:
+                heapq.heappush(max_h,el)
             else:
-                while abs(len(minH) - len(maxH)) != 1:
-                    if len(minH) > len(maxH):
-                        heapq.heappush(maxH, heapq.heappop(minH))
-                    heapq.heappush(minH, heapq.heappop(maxH))
-    return m
+                heapq.heappush(min_h,el)
+                
+            temp = get_median(max_h,min_h)
+            
+        li_m.append(temp)
 
-li = [5,3,4,1,6]
-print medians(li)
+    return li_m
+    
+if __name__ == "__main__":
+
+    li = [5,3,4,1,6]
+    # expected output >> [5,4,4,3.5,4]
+    li2 = [5,15,1,3]
+    # expected output >> [5,10,5,4]
+    li3 = [12,7,8,11]
+    # expected output >> [12,9.5,8,9.5]
+
+    print medians(li)
+    print medians(li2)
+    print medians(li3)
